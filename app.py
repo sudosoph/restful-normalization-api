7ab2c868bb1c9a5757147734d4b18bf339c6cbdd
Sophia <soph@soph.codes>
2021-03-15 15:39:05 -0400

    Iterating through files

--- app.py
+++ app.py
@@ -9,8 +9,18 @@ app = Flask(__name__)
 api = Api(app)
 
 @app.route('/', methods=['POST'])
-def post():
+def post():   
+    if request.method == 'POST':
+        data = request.args.getlist('files')
+    # check if array is empty or not
+    if data:    
+       # iterate a received array
+        for arg in data:
+            # print every single filename
+            return "{}\n".format(arg)
+    else: 
+         # if empty, return {'status': 'ok'}
         return {'status': 'ok'}
-
+    
 if __name__ == '__main__':
     api.run(debug=True, port=5000)
