4ebf827f5bd7fee2998909b0e7a35f805cd1bdb3
Sophia <soph@soph.codes>
2021-03-22 11:05:15 -0400

    Added sample files from testbed and cleaned up test.py

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import requests
 
 URL = "http://127.0.0.1:5000/"
-obj = {"files": ["test1.wav", "test2.wav"]}
+obj = {
+    "files": [
+        "test-audios/M5_b49_w5_orig.wav",
+        "test-audios/M5_b49_w6_orig.wav",
+        "test-audios/M5_b50_w1_orig.wav",
+        "test-audios/M5_b50_w2_orig.wav",
+        "test-audios/M5_b50_w3_orig.wav",
+        "test-audios/M5_b50_w4_orig.wav",
+    ]
+}
 
 responses = requests.post(URL, json=obj)
 print(responses.text)
