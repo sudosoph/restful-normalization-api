cd22d598cd1819bed85e60e0793118883b2690c2
Sophia <soph@soph.codes>
2021-04-19 11:43:06 -0400

    Logging and testing for impaired audio

--- app.py
+++ app.py
@@ -5,19 +5,27 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from pydub import AudioSegment, effects
+import logging
 
 
 app = Flask(__name__)
 api = Api(app)
 
+def match_target_amplitude(sound, target_dBFS):
+    change_in_dBFS = target_dBFS - sound.dBFS
+    return sound.apply_gain(change_in_dBFS)
 
 @app.route("/", methods=["POST"])
 def post():
     files = request.get_json(force=True)
     for i in files["files"]:
         rawsound = AudioSegment.from_wav("test-audios/" + i)
-        normalizedsound = effects.normalize(rawsound)
-        normalizedsound.export("test-audios/output/" + i)
+        #normalizedsound = effects.normalize(rawsound)
+        l = logging.getLogger("pydub.converter")
+        l.setLevel(logging.DEBUG)
+        l.addHandler(logging.StreamHandler())
+        normalized_sound = match_target_amplitude(rawsound, -5.0)
+        normalized_sound.export("test-audios/output/" + i)
     return {"status": "ok"}
 
 
--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import requests
 URL = "http://127.0.0.1:5000/"
 obj = {
     "files": [
-        "M5_b49_w5_orig.wav",
-        "M5_b49_w6_orig.wav",
-        "M5_b50_w1_orig.wav",
-        "M5_b50_w2_orig.wav",
-        "M5_b50_w3_orig.wav",
-        "M5_b50_w4_orig.wav",
+        #"M5_b49_w5_orig.wav",
+        #"M5_b49_w6_orig.wav",
+        #"M5_b50_w1_orig.wav",
+        #"M5_b50_w2_orig.wav",
+        #"M5_b50_w3_orig.wav",
+        #"M5_b50_w4_orig.wav",
         "impaired_test_file.wav"
     ]
 }
